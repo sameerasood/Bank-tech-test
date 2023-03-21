950d7012bbc553c415631a7b9631275394051c20
sameera sood <sameerasood@gmail.com>
2023-03-21 11:32:05 +0000

    test passed for multiple transactions

--- _tests_/transaction.test.js
+++ _tests_/transaction.test.js
@@ -20,16 +20,20 @@ describe("TRANSACTION class", () => {
   it("returns balance with added credit", () => {
     const transaction = new Transaction();
     const mockCredit = new Credit(100, "21/03/2023");
+    
     mockCredit.getCredit.mockImplementation(() => 100); // mock implementation of getCredit function
     transaction.addCredit(mockCredit);
+    
     expect(transaction.getBalance()).toEqual(100);
   });
 
   it("returns date of credit transaction", () => {
     const transaction = new Transaction();
     const mockCredit = new Credit(100, "21/03/2023");
+    
     mockCredit.getDate.mockImplementation(() => "21/03/2023");
     transaction.addCredit(mockCredit);
+    
     expect(transaction.transactionDate()).toEqual("21/03/2023");
   });
 
@@ -37,26 +41,47 @@ describe("TRANSACTION class", () => {
     const transaction = new Transaction();
     const mockCredit1 = new Credit(100, "21/03/2023");
     const mockCredit2 = new Credit(400, "16/05/2023");
+    
     mockCredit1.getCredit.mockImplementation(() => 100);
     mockCredit2.getCredit.mockImplementation(() => 400);
+    
     transaction.addCredit(mockCredit1);
     transaction.addCredit(mockCredit2);
+    
     expect(transaction.getBalance()).toEqual(500);
   });
 
   it("returns balance in negative when withdraw happens", () => {
     const transaction = new Transaction();
     const mockDebit = new Debit(100, "21/03/2023");
+    
     mockDebit.getDebit.mockImplementation(() => 100);
     transaction.withdrawDebit(mockDebit);
+    
     expect(transaction.getBalance()).toEqual(-100);
   });
 
-  it("returns returns date of debit transaction", () => {
+  it("returns date of debit transaction", () => {
     const transaction = new Transaction();
     const mockDebit = new Debit(100, "21/03/2023");
+    
     mockDebit.getDate.mockImplementation(() => "21/03/2023");
     transaction.withdrawDebit(mockDebit);
+    
     expect(transaction.transactionDate()).toEqual("21/03/2023");
+  });
+
+  it("returns total balance after one credit and one debit transaction", () => {
+    const transaction = new Transaction();
+    const mockCredit = new Credit(1000, "16/03/2023");
+    const mockDebit = new Debit(150, "21/03/2023");
+
+    mockCredit.getCredit.mockImplementation(() => 1000);
+    transaction.addCredit(mockCredit);
+
+    mockDebit.getDebit.mockImplementation(() => 150);
+    transaction.withdrawDebit(mockDebit);
+
+    expect(transaction.getBalance()).toEqual(850);
   });
 });
