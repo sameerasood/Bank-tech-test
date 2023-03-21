0638173596947b21f452c16b3448d7549e8931a7
sameera sood <sameerasood@gmail.com>
2023-03-21 10:45:38 +0000

    returns the initial balance 0 test cleared

--- _tests_/transaction.test.js
+++ _tests_/transaction.test.js
@@ -1,7 +1,19 @@
-const Balance = require("./src/balance");
+const Transaction = require("../src/transaction");
+const Credit = require("../src/credit");
+const Debit = require("../src/debit");
 
-describe("Balance class", () => {
-  it("returns the balance", () => {
-    expect();
+jest.mock("../src/credit"); // Credit is now a mock constructor
+jest.mock("../src/debit"); // Debit is now a mock constructor
+
+describe("TRANSACTION class", () => {
+  beforeEach(() => {
+    // Clear all instances and calls to constructor and all methods:
+    Credit.mockClear();
+    Debit.mockClear();
+  });
+
+  it("returns starting balance = 0", () => {
+    const transaction = new Transaction();
+    expect(transaction.getBalance()).toBe(0);
   });
 });
